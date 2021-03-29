394abc61f2713cca5d961191e9a6d54055cf41af
Antoine <antoine.thenevin@gmail.com>
2021-03-29 17:16:08 +0200

    Pool works !

--- app/main.cpp
+++ app/main.cpp
@@ -6,6 +6,11 @@
 
 #include <PongScene.hpp>
 
+#include <Pool.hpp>
+#include <iostream>
+#define TRACE_METHOD() std::cout << this << " " << "test" << "\n";
+void testPools();
+
 int main(int argc, char** argv) {
   cxxopts::Options options(argv[0], "A pong game which use a simple game engine!");
 
@@ -18,5 +23,27 @@ int main(int argc, char** argv) {
   // On lance le jeu
   pong.run();
 
+  testPools();
   return 0;
+}
+
+
+struct Foo {
+  int x = 42;
+  Foo() { TRACE_METHOD(); }
+  Foo(int x) : x(x) { TRACE_METHOD(); }
+  ~Foo() { TRACE_METHOD(); };
+};
+
+void testPools(){
+  minipool<Foo> mp(256);
+
+  Foo *p1 = mp.alloc();
+  Foo *p2 = mp.alloc(44);
+
+  std::cout << "p1->x=" << p1->x << "\n";
+  std::cout << "p2->x=" << p2->x << "\n";
+
+  mp.free(p1);
+  mp.free(p2);
 }
--- include/Pool.hpp
+++ include/Pool.hpp
@@ -2,20 +2,19 @@
 
 #include <memory>
 #include <type_traits>
+#include <cassert>
 
 // to allocate pools of object
 template <typename T> struct minipool {
 
-};
-
-template <typename T> union minipool_item {
+    union minipool_item {
     public:
         // Methods for the list of free items.
         minipool_item *get_next_item() const { return next; }
         void set_next_item(minipool_item *n) { next = n; }
 
         // Methods for the storage of the item.
-        T *get_storage() { return reinterpret_cast<T *>(datum); }
+        T *get_storage() { return reinterpret_cast<T *>(&datum); }
 
         // Given a T* cast it to a minipool_item*
         static minipool_item *storage_to_item(T *t) {
@@ -32,13 +31,15 @@ template <typename T> union minipool_item {
         StorageType datum;
 }; // minipool_item
 
-template <typename T> struct minipool_arena {
+struct minipool_arena {
     private:
         // Storage of this arena.
-        std::unique_ptr<minipool_item<T>[]> storage;
+        std::unique_ptr<minipool_item[]> storage;
         // Pointer to the next arena.
         std::unique_ptr<minipool_arena> next;
         // Creates an arena with arena_size items.
+    
+    public:
         minipool_arena(size_t arena_size) : storage(new minipool_item[arena_size]) {
             for (size_t i = 1; i < arena_size; i++) {
                 storage[i - 1].set_next_item(&storage[i]);
@@ -56,3 +57,58 @@ template <typename T> struct minipool_arena {
             next.reset(n.release());
         }
 }; // minipool_arena
+
+    // Size of the arenas created by the pool.
+    size_t arena_size;
+    // Current arena. Changes when it becomes full and we want to allocate one
+    // more object.
+    std::unique_ptr<minipool_arena> arena;
+    // List of free elements. The list can be threaded between different arenas
+    // depending on the deallocation pattern.
+    minipool_item *free_list;
+
+    // Creates a new pool that will use arenas of arena_size.
+    minipool(size_t arena_size) : arena_size(arena_size), arena(new minipool_arena(arena_size)), free_list(arena->get_storage()) {}
+
+    // Allocates an object in the current arena.
+    template <typename... Args> T *alloc(Args &&... args) {
+        if (free_list == nullptr) {
+            // If the current arena is full, create a new one.
+            std::unique_ptr<minipool_arena> new_arena(new minipool_arena(arena_size));
+            // Link the new arena to the current one.
+            new_arena->set_next_arena(std::move(arena));
+            // Make the new arena the current one.
+            arena.reset(new_arena.release());
+            // Update the free_list with the storage of the just created arena.
+            free_list = arena->get_storage();
+        }
+
+        // Get the first free item.
+        minipool_item *current_item = free_list;
+        // Update the free list to the next free item.
+        free_list = current_item->get_next_item();
+
+        // Get the storage for T.
+        T *result = current_item->get_storage();
+        // Construct the object in the obtained storage.
+        new (result) T(std::forward<Args>(args)...);
+
+        return result;
+    }
+
+    void free(T *t) {
+        // Destroy the object.
+        t->T::~T();
+
+        // Convert this pointer to T to its enclosing pointer of an item of the
+        // arena.
+        minipool_item *current_item = minipool_item::storage_to_item(t);
+
+        // Add the item at the beginning of the free list.
+        current_item->set_next_item(free_list);
+        free_list = current_item;
+    }
+}; // minipool<T>
+
+
+
