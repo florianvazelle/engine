f2868672e210bef4ac54079d4dc6e176eca38405
Shen <s_lin97@hotmail.com>
2021-03-30 12:14:24 +0200

    RTTI Done

--- src/Common/RTTI.cpp
+++ src/Common/RTTI.cpp
@@ -1,4 +1,23 @@
 #include <Engine/Common/RTTI.hpp>
 
-bool RTTI::IsExactly (const RTTI & rtti ) const {return this == &rtti;}
-bool RTTI::IsA(const RTTI& rtti) const {return false;}  // IsExactly () recursif
+RTTI RTTI::Default("", *((RTTI*)nullptr));
+
+RTTI::RTTI(const char* name, const RTTI& parent_type) : m_Parent(parent_type) {
+  m_ClassName = name;
+  // type = fnv1a(name);
+}
+
+bool RTTI::IsExactly(const RTTI& other) const { return m_ClassName == other.m_ClassName; }
+
+bool RTTI::IsA(const RTTI& other) const 
+{
+          const RTTI* p = this;
+          do 
+          {
+            if (p->IsExactly(other)) return true;
+            p = &(p->m_Parent);
+          } while (p != nullptr);
+
+          return false;
+}  // IsExactly () recursif
+
