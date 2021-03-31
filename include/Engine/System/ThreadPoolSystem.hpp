8074d058a4f38a52b8c074e67bbfffd0b39c55dd
Shen <s_lin97@hotmail.com>
2021-03-31 11:02:15 +0200

    ThreadPoll class Done
    pass to the testing step

--- include/Engine/System/ThreadPoolSystem.hpp
+++ include/Engine/System/ThreadPoolSystem.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
 #include <Engine/System/System.hpp>
+#include <vector>
+#include <thread>
+#include <future>
+#include <queue>
 
 class ThreadPoolSystem : public System {
-public:
-  void update(double deltaTime);
 
+  private:
+  std::queue<std::function<void()>> m_function_queue;
+  std::mutex m_lock;
+  std::condition_variable m_data_condition;
+  std::atomic<bool> m_accept_functions;
+
+public:
+
+  ThreadPoolSystem();
+  ~ThreadPoolSystem();
+  void push(std::function<void()> func);
+  void done();
+  void infinite_loop_func();
 };
--- src/System/ThreadPoolSystem.cpp
+++ src/System/ThreadPoolSystem.cpp
@@ -1,1 +1,42 @@
 #include <Engine/System/ThreadPoolSystem.hpp>
+
+
+ThreadPoolSystem::ThreadPoolSystem() : m_function_queue(), m_lock(), m_data_condition(), m_accept_functions(true) {}
+
+ThreadPoolSystem::~ThreadPoolSystem() {}
+
+void ThreadPoolSystem::push(std::function<void()> func) {
+  std::unique_lock<std::mutex> lock(m_lock);
+  m_function_queue.push(func);
+  // when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+  lock.unlock();
+  m_data_condition.notify_one();
+}
+
+void ThreadPoolSystem::done() {
+  std::unique_lock<std::mutex> lock(m_lock);
+  m_accept_functions = false;
+  lock.unlock();
+  // when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+  m_data_condition.notify_all();
+  // notify all waiting threads.
+}
+
+void ThreadPoolSystem::infinite_loop_func() {
+  std::function<void()> func;
+  while (true) {
+    {
+      std::unique_lock<std::mutex> lock(m_lock);
+      m_data_condition.wait(lock, [this]() { return !m_function_queue.empty() || !m_accept_functions; });
+      if (!m_accept_functions && m_function_queue.empty()) {
+        // lock will be release automatically.
+        // finish the thread loop and let it join in the main thread.
+        return;
+      }
+      func = m_function_queue.front();
+      m_function_queue.pop();
+      // release the lock
+    }
+    func();
+  }
+}
